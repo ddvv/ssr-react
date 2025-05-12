92533a73792d7c4174f0250b9925ee9ee6102a3a
Dmitriy Danilov <dima-danilov1995@gmail.com>
2025-05-12 23:07:35 +0300

    Настройка сервера для разработки с помощью Vite

--- server.js
+++ server.js
@@ -2,23 +2,54 @@ import express from 'express'
 import fs from "node:fs/promises";
 import sirv from 'sirv'
 
-const app = express()
-
+const isProduction = process.env.NODE_ENV === 'production';
+const isDevelopment = process.env.NODE_ENV === 'development';
 const PORT = 3000;
 const BASE = '/'
-const TEMPLATE =  await fs.readFile('./dist/client/index.html', 'utf-8');
 
-app.use(BASE, sirv('./dist/client', { extensions: [] }))
+const app = express()
+
+/** @type {import('vite').ViteDevServer | undefined} */
+let vite;
+
+if (isDevelopment) {
+    const {createServer} = await import('vite')
+    vite = await createServer({
+        server: {middlewareMode: true},
+        appType: 'custom',
+        BASE,
+    })
+    app.use(vite.middlewares)
+}
+
+if (isProduction) {
+    app.use(BASE, sirv('./dist/client', {extensions: []}))
+}
 
 app.use('*all', async (req, res) => {
     const url = req.originalUrl;
+    /** @type {string} */
+    let template;
+    /** @type {import('./src/entry-server.ts').render} */
     let render;
-    render = (await import('./dist/server/entry-server.js')).render;
+
+    if (isDevelopment) {
+        template = await fs.readFile('./index.html', 'utf-8');
+        template = await vite.transformIndexHtml(url, template);
+        render = (await vite.ssrLoadModule('/src/entry-server.tsx')).render;
+    }
+
+    if (isProduction) {
+        template = await fs.readFile('./dist/client/index.html', 'utf-8');
+        render = (await import('./dist/server/entry-server.js')).render;
+    }
+
     const rendered = await render(url);
-    const html = TEMPLATE
+    const html = template
         .replace(`<!--app-head-->`, rendered.head ?? '')
-        .replace(`<!--app-html-->`, rendered.html ?? '')
-    res.status(200).set({ 'Content-Type': 'text/html' }).send(html)
+        .replace(`<!--app-html-->`, rendered.html ?? '');
+
+    res.status(200).set({'Content-Type': 'text/html'}).send(html)
 });
 
 app.listen(PORT, () => {
